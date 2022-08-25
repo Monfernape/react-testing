7958281cf749bba190ba27cd41c45767e6acdca1
Monfernape <engr.usmankhalil@gmail.com>
2022-08-25 19:13:01 +0500

    Add testing

---
+++ src/main/Counter.test.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Counter } from "./Counter";
+
+it("should render a counter with value of 1", () => {
+  render(<Counter />);
+  let basicCounter = screen.getByRole("alert");
+  expect(basicCounter).toHaveValue(1);
+});
+
+// it("should increase count when plus button is clicked", () => {
+//   expect(basicCounter).toHaveValue(1);
+//   userEvent.click(increaseCount);
+//   expect(basicCounter).toHaveValue(2);
+// });
+
+// it("should decrease count when minus button is clicked", () => {
+//   userEvent.click(increaseCount);
+//   expect(basicCounter).toHaveValue(2);
+//   userEvent.click(decreaseCount);
+//   expect(basicCounter).toHaveValue(1);
+// });
+
+// it("should not decrease to less than 1", () => {
+//   expect(basicCounter).toHaveValue(1);
+//   userEvent.click(decreaseCount);
+//   expect(basicCounter).toHaveValue(1);
+// });
