ec80f4546577691de0f658d5814bc49c6e2ac134
Monfernape <engr.usmankhalil@gmail.com>
2022-08-31 20:11:09 +0500

    Testing on specific directory

---
+++ view/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Counter } from "./main/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter title="counters" />
+    </div>
+  );
+}
+
+export default App;
--- view/src/main/CounterTest.test.jsx
+++ view/src/main/CounterTest.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import { Counter } from "./Counter";
 import userEvent from "@testing-library/user-event";
 
-it("should render a counter with value of 1", () => {
+it("should render a value of 1", () => {
   render(<Counter />);
   const basicCounter = screen.getByRole("alert");
   expect(basicCounter).toHaveValue(1);
