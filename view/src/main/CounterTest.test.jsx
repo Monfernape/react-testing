b54de8d9d06e7799b4ebd58132147c94ad4692a6
Monfernape <engr.usmankhalil@gmail.com>
2022-08-31 20:16:16 +0500

    tesing the updated counter

--- view/src/main/CounterTest.test.jsx
+++ view/src/main/CounterTest.test.jsx
@@ -8,14 +8,3 @@ it("should render a value of 1", () => {
   const basicCounter = screen.getByRole("alert");
   expect(basicCounter).toHaveValue(1);
 });
-
-it("should increase count when plus button is clicked", () => {
-  const basicCounter = screen.getByRole("alert");
-  const increaseCount = screen.getByRole("button", {
-    name: "Increment by one",
-  });
-
-  expect(basicCounter).toHaveValue(1);
-  userEvent.click(increaseCount);
-  expect(basicCounter).toHaveValue(2);
-});
